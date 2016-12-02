fe9bd75235a770639e6b1c80930ce1898db34971
Myra Lukens <myralukens@gmail.com>
2016-12-02 13:32:57 +0100

    scroll animation to top

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-(function() {
-
-
-}).call(this);
+$(document).ready(function(){
+   
+   //Check to see if the window is top if not then display button
+   $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+         $('.scrollup').fadeIn();
+      } else {
+         $('.scrollup').fadeOut();
+      }
+   });
+   
+   //Click event to scroll to top
+   $('.scrollup').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+   });
+   
+});
